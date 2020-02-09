c3543e6f47cc0cd150f1d11ac68ce074891dff1e
samanayubi@gmail.com <samanayubi@gmail.com>
2020-02-10 02:48:37 +0530

    new variables added

--- src/app/global-constants.ts
+++ src/app/global-constants.ts
@@ -1,65 +1,69 @@
 export const GLOBAL_CONST = {
 
-    ROUTE_PARAMS: {
-      DISCLAIMER: 'disclaimer',
-      HOME: 'home'
-      
-    },
+  ROUTE_PARAMS: {
+    DISCLAIMER: 'disclaimer',
+    HOME: 'home'
+    
+  },
 
-    TABLES: {
-      HISTORY_TABLE: {
-        TABLE_HEADER: [{
-            id: "position",
-            value: "No."
-          }, {
-            id: "from",
-            value: "From"
-          },
-          {
-            id: "to",
-            value: "To"
-          },
-          {
-            id: "date",
-            value: "Date"
-          },
-          {
-           id: "time",
-           value: "Time"
-         }
-         ]
-      } 
-    },
+  TABLES: {
+    HISTORY_TABLE: {
+      TABLE_HEADER: [{
+          id: "position",
+          value: "No."
+        }, {
+          id: "from",
+          value: "From"
+        },
+        {
+          id: "to",
+          value: "To"
+        },
+        {
+          id: "date",
+          value: "Date"
+        },
+        {
+         id: "time",
+         value: "Time"
+       }
+       ]
+    } 
+  },
 
-     SELECT_VALUE:{
-     currency : [
-      {value: 'CAD', viewValue: 'CAD'},
-      {value: 'USD', viewValue: 'USD'},
-      {value: 'EUR', viewValue: 'EUR'}
-    ]
+   SELECT_VALUE:{
+   currency : [
+    {value: 'CAD', viewValue: 'CAD'},
+    {value: 'USD', viewValue: 'USD'},
+    {value: 'EUR', viewValue: 'EUR'}
+  ]
 
-   
- },
+ 
+},
 
 
- API_URLS:{
-     CURRENCY_EXCHNGE_API_URL: 'https://api.exchangeratesapi.io/latest/'
+API_URLS:{
+   CURRENCY_EXCHNGE_API_URL: 'https://api.exchangeratesapi.io/latest/'
 
-    },
+  },
 
-    TEXTS :{
-        BUTTON: 'Back',
-        DISCLAIMER_LABEL: 'Disclaimer',
-        DISCLAIMER_TEXT: 'The details expressed within this website and accompanying documents or transmissions are for information purposes only and are not intended as a solicitation for funds or a recommendation to trade. exchangeratesapi.io/ accepts no liability whatsoever for any loss or damages suffered through any act or omission taken as a result of reading or interpreting any of the information contained or related to this site.'
-    }
-   
-   
-   
+  TEXTS :{
+    BUTTON: 'Back',
+    DISCLAIMER_LABEL: 'Disclaimer',
+    TO_AMT_LABEL: 'To Amount',
+    FROM_AMT : 'From Amount',
+    CONVERTOR_LABEL:'Currency Converter',
+    HISTORY_TABLE_HEADER: 'Transaction History Data',
+    DISCLAIMER_TEXT: 'The details expressed within this website and accompanying documents or transmissions are for information purposes only and are not intended as a solicitation for funds or a recommendation to trade. exchangeratesapi.io/ accepts no liability whatsoever for any loss or damages suffered through any act or omission taken as a result of reading or interpreting any of the information contained or related to this site.'
+  }
+ 
+ 
+ 
+
   
-    
-   
-    
-    
-    
-   
-  };
+ 
+  
+  
+  
+ 
+};
